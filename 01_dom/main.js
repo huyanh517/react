c0fc5c71970ba71fc3f1ce0a0afff8766b0e159b
Huy Anh <huyanh151998@gmail.com>
2021-08-18 15:09:39 +0700

    asd

--- 01_dom/main.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector("#fullname")
-const result = document.querySelector(".text")
-
-input.addEventListener("input", event => {
-  result.innerText = event.target.value
-})
