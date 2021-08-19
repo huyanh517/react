1d1475bc641ae8084a48bc0daa3b18d7d5c0bac3
Huy Anh <huyanh151998@gmail.com>
2021-08-20 05:52:41 +0700

    reuse fixed

--- 03-blogs/src/components/ArticleItem/ArticleDateTime.js
+++ 03-blogs/src/components/ArticleItem/ArticleDateTime.js
@@ -8,7 +8,7 @@ export const ArticleDateTime = () => {
         <svg
           stroke="currentColor"
           fill="currentColor"
-          stroke-width="0"
+          strokeWidth="0"
           viewBox="0 0 512 512"
           className="css-uk6cul"
           height="1em"
