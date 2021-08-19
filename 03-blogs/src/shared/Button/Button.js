dd772d5c4e055847cf80403b031029b69cdee4c5
Huy Anh <huyanh151998@gmail.com>
2021-08-19 09:39:07 +0700

    reuse-button

--- 03-blogs/src/shared/Button/Button.js
+++ 03-blogs/src/shared/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({ icon, value, className }) => {
   return (
-    <button className={`btn ${className}`} icon>
+    <button className={`btn ${className}`} icon={null}>
       {icon}
       {value}
     </button>
