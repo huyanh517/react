ad9a484e7c3cee283c9bddb09d6e6c2223d8d474
Huy Anh <huyanh151998@gmail.com>
2021-08-28 10:44:37 +0700

    router

--- 03-blogs/src/components/ArticleItem/ArticleTitle.js
+++ 03-blogs/src/components/ArticleItem/ArticleTitle.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const ArticleTitle = () => {
   return (
     <h2 className="article-item__title">
-      <a href="/post-detail.html"
-      >Only Someone Who's Seen The Mummy Will Pass This</a
-      >
+      <Link to="/post/detail"
+      >Only Someone Who's Seen The Mummy Will Pass This
+      </Link>
     </h2>
   )
 }
